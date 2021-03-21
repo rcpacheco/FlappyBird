e2651f5807409669d4e2d3e6a6f645a15cf76227
Ricardo Pacheco <ricardo.pacheco.roldan@gmail.com>
2021-03-21 07:03:09 -0600

    Video 1

---
+++ FlappyBird.Web/Models/BirdModel.cs
@@ -0,0 +1,12 @@
+﻿namespace FlappyBird.Web.Models
+{
+    public class BirdModel
+    {
+        public int DistanceFromGround { get; set; } = 100;
+
+        public void Fall(int gravity)
+        {
+            DistanceFromGround -= gravity;
+        }
+    }
+}
---
+++ FlappyBird.Web/Models/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace FlappyBird.Web.Models
+{
+    public class GameManager : INotifyPropertyChanged
+    {
+        private readonly int _gravity = 2;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public BirdModel Bird { get; set; }
+
+        public bool IsRunning { get; set; } = false;
+
+        public GameManager()
+        {
+            Bird = new BirdModel();
+        }
+
+        public async void MainLoop()
+        {
+            IsRunning = true;
+            while (IsRunning)
+            {
+                Bird.Fall(_gravity);
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Bird)));
+
+                if (Bird.DistanceFromGround <= 0)
+                {
+                    IsRunning = false;
+                }
+                await Task.Delay(20).ConfigureAwait(false);
+            }
+        }
+
+        public void StartGame()
+        {
+            if (!IsRunning)
+            {
+                Bird = new BirdModel();
+                MainLoop();
+            }
+        }
+    }
+}

d391d8dac181d529bd6240980579c6c38c6538f1
Ricardo Pacheco <ricardo.pacheco.roldan@gmail.com>
2021-03-21 07:12:25 -0600

    Agrego pipe

---
+++ FlappyBird.Web/Models/PipeModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FlappyBird.Web.Models
+{
+    public class PipeModel
+    {
+
+        public int DistanceFromLeft { get; set; } = 500;
+
+        public int DistanceFromBottom { get; set; } = new Random().Next(0, 60);
+    }
+}
